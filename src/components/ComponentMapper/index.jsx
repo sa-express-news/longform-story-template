b5f5f08fd367ea2d3a11ce6eef0fccccc584040b
Kia Farhang <kfarhang0@gmail.com>
2017-09-04 19:22:50 -0500

    Removed a bunch of components not required in the template

--- src/components/ComponentMapper/index.jsx
+++ src/components/ComponentMapper/index.jsx
@@ -13,55 +13,54 @@ import SmallPhotoDesktopContainerReverse from '../SmallPhotoDesktopContainerReve
 import Photos from '../Photos';
 import Slideshow from '../Slideshow';
 import Subscribe from '../Subscribe';
-import PhotoEssayContainer from '../PhotoEssayContainer';
 import Annotation from '../Annotation';
 import Annotations from '../Annotations';
-import PullQuote from '../PullQuote'; 
+import PullQuote from '../PullQuote';
 import RelatedContent from '../RelatedContent';
 import ResponsiveiFrame from '../ResponsiveiFrame';
 import Video from '../Video';
 import Credits from '../Credits';
 
-const loadPhoto = (path: string) => {
-	return require(`${path}`);
-};
+// const loadPhoto = (path: string) => {
+// 	return require(`${path}`);
+// };
 
-export default{
-	text: (object: Object, key: number) => <Paragraph text={object.value} key={key}/>,
+export default {
+	text: (object: Object, key: number) => <Paragraph text={object.value} key={key} />,
 
-	intrograph: (object: Object, key: number) => <IntroParagraph text={object.value.text} key={key}/>,
+	intrograph: (object: Object, key: number) => <IntroParagraph text={object.value.text} key={key} />,
 
-	credits: (object: Object, key: number) => <Credits credits={object.value} key={key}/>,
+	credits: (object: Object, key: number) => <Credits credits={object.value} key={key} />,
 
-	sectionheading: (object: Object, key: number) => <SectionHeading text={object.value} key={key}/>,
+	sectionheading: (object: Object, key: number) => <SectionHeading text={object.value} key={key} />,
 
-	photo: function(object: Object, key: number){
+	photo: function (object: Object, key: number) {
 		const photo = object.value;
 		const photoPath = photo.source;
-		switch (photo.type){
-			
-			case 'full': 
-			let photoComponent = <FullPhoto src={photoPath} alt={photo.caption}/>;
-			return <FullPhotoContainer caption={photo.caption} cutline={photo.cutline} key={key}>{photoComponent}</FullPhotoContainer>
-			
+		switch (photo.type) {
+
+			case 'full':
+				let photoComponent = <FullPhoto src={photoPath} alt={photo.caption} />;
+				return <FullPhotoContainer caption={photo.caption} cutline={photo.cutline} key={key}>{photoComponent}</FullPhotoContainer>
+
 			case 'small-left':
-			let smallPhotoComponent = <SmallPhotoDesktop src={photoPath} alt={photo.caption}/>
-			return <SmallPhotoDesktopContainer caption={photo.caption} cutline={photo.cutline} key={key}>{smallPhotoComponent}</SmallPhotoDesktopContainer>
-			
+				let smallPhotoComponent = <SmallPhotoDesktop src={photoPath} alt={photo.caption} />
+				return <SmallPhotoDesktopContainer caption={photo.caption} cutline={photo.cutline} key={key}>{smallPhotoComponent}</SmallPhotoDesktopContainer>
+
 			case 'small-right':
-			let smallPhotoComponentReverse = <SmallPhotoDesktop src={photoPath} alt={photo.caption}/>
-			return <SmallPhotoDesktopContainerReverse caption={photo.caption} cutline={photo.cutline} key={key}>{smallPhotoComponentReverse}</SmallPhotoDesktopContainerReverse>
+				let smallPhotoComponentReverse = <SmallPhotoDesktop src={photoPath} alt={photo.caption} />
+				return <SmallPhotoDesktopContainerReverse caption={photo.caption} cutline={photo.cutline} key={key}>{smallPhotoComponentReverse}</SmallPhotoDesktopContainerReverse>
 
 			default:
-			return null
+				return null
 		}
 	},
-	
-	photos: (object: Object, key: number) => <Photos photos={object.value} key={key}/>,
 
-	slideshow: (object: Object, key: number) => <Slideshow photos={object.value} key={key}/>,
+	photos: (object: Object, key: number) => <Photos photos={object.value} key={key} />,
 
-	loadPhoto: function(path: string){
+	slideshow: (object: Object, key: number) => <Slideshow photos={object.value} key={key} />,
+
+	loadPhoto: function (path: string) {
 		return require(`${path}`);
 	},
 
@@ -69,22 +68,20 @@ export default{
 
 	annotations: (object: Object, key: number) => <Annotations annotations={object.value} key={key} />,
 
-	photoessay: (object: Object, key: number) => <PhotoEssayContainer photos={object.value} key={key} loadPhoto={loadPhoto} />,
-
 	pullquote: (object: Object, key: number) => <PullQuote quote={object.value.quote} source={object.value.source} key={key} />,
 
 	subscribe: (object: Object, key: number) => <Subscribe link={object.value.link} key={key} />,
 
 	related: (object: Object, key: number) => <RelatedContent headline={object.value.headline} link={object.value.link} key={key} />,
 
-	iframe: (object: Object, key: number) => <ResponsiveiFrame src={object.value.source} key={key}/>,
+	iframe: (object: Object, key: number) => <ResponsiveiFrame src={object.value.source} key={key} />,
 
-	video: function(object: Object, key: number){
+	video: function (object: Object, key: number) {
 		const video = object.value;
-		return <Video src={video.source} autoPlay={video.autoplay} muted={video.muted} loop={video.loop} controls={video.controls}/>
+		return <Video src={video.source} autoPlay={video.autoplay} muted={video.muted} loop={video.loop} controls={video.controls} />
 	},
 
-	renderComponent: function(object: Object, key: number){
+	renderComponent: function (object: Object, key: number) {
 		return object.type && this[object.type] ? this[object.type](object, key) : null;
 	}
 };
