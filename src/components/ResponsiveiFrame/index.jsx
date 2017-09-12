f69a815b6453787d711d2d60a9767b1baca5040d
Kia Farhang <kfarhang0@gmail.com>
2017-09-12 16:16:23 -0500

    Added iFrame component to dummy story AML

--- src/components/ResponsiveiFrame/index.jsx
+++ src/components/ResponsiveiFrame/index.jsx
@@ -1,21 +1,29 @@
 // @flow
 
-import React, {Component} from 'react';
+import React from 'react';
 
 import './ResponsiveiFrame.css';
 
-class ResponsiveiFrame extends Component {
-	props: {
-		src: string,
-	};
-
-	render(){
-		return(
-			<div className='iFrameContainer'>
-				<iframe src={this.props.src} frameBorder="0" allowFullScreen="true" />		
-			</div>
-		)
-	}
+const ResponsiveiFrame = (props) => {
+	return (
+		<div className='iFrameContainer'>
+			<iframe src={props.src} frameBorder='0' allowFullScreen='true' />
+		</div>
+	)
 }
+
+// class ResponsiveiFrame extends Component {
+// 	props: {
+// 		src: string,
+// 	};
+
+// 	render() {
+// 		return (
+// 			<div className='iFrameContainer'>
+// 				<iframe src={this.props.src} frameBorder="0" allowFullScreen="true" />
+// 			</div>
+// 		)
+// 	}
+// }
 
 export default ResponsiveiFrame;
